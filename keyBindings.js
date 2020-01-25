2542071e21ac060ece61174128595dc6449d5237
Matthew <barbaramatthew90@gmail.com>
2020-01-25 08:39:42 +0100

    Fixed controller smoothness

--- keyBindings.js
+++ keyBindings.js
@@ -1,32 +1,72 @@
 /** @format */
+
+const KEYS = {
+  A: 65,
+  ARROW_LEFT: 37,
+  W: 87,
+  ARROW_UP: 38,
+  D: 68,
+  ARROW_RIGHT: 39,
+  S: 83,
+  ARROW_DOWN: 40
+};
+
 document.addEventListener("DOMContentLoaded", function(event) {
+  // reset moving speed to 0
+  document.querySelector("body").onkeyup = e => {
+    switch (e.which) {
+      case KEYS.A:
+      case KEYS.ARROW_LEFT: {
+        myGamePiece.speedX = 0;
+        break;
+      }
+
+      case KEYS.W:
+      case KEYS.ARROW_UP: {
+        myGamePiece.speedY = 0;
+        break;
+      }
+
+      case KEYS.D:
+      case KEYS.ARROW_RIGHT: {
+        myGamePiece.speedX = 0;
+        break;
+      }
+
+      case KEYS.S:
+      case KEYS.ARROW_DOWN: {
+        myGamePiece.speedY = 0;
+        break;
+      }
+
+      default:
+        return; // exit this handler for other keys
+    }
+  };
+
   document.querySelector("body").onkeydown = e => {
     switch (e.which) {
-      // arrow left
-      case 65:
-      case 37: {
-        myGamePiece.speedX -= 0.5;
+      case KEYS.A:
+      case KEYS.ARROW_LEFT: {
+        myGamePiece.speedX = -2;
         break;
       }
 
-      // arrow up
-      case 87:
-      case 38: {
-        myGamePiece.speedY -= 0.5;
+      case KEYS.W:
+      case KEYS.ARROW_UP: {
+        myGamePiece.speedY = -2;
         break;
       }
 
-      // arrow right
-      case 68:
-      case 39: {
-        myGamePiece.speedX += 0.5;
+      case KEYS.D:
+      case KEYS.ARROW_RIGHT: {
+        myGamePiece.speedX = 2;
         break;
       }
 
-      // arrow down
-      case 83:
-      case 40: {
-        myGamePiece.speedY += 0.5;
+      case KEYS.S:
+      case KEYS.ARROW_DOWN: {
+        myGamePiece.speedY = 2;
         break;
       }
 
